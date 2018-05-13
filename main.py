e4c0cc0872b5cbee0dc8ad817addc7fe92a8922c
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-05-14 03:22:25 +0700

    Updates

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('lenna.png')
 
-blur = cv2.GaussianBlur(img,(51,51),0)
+edges = cv2.Canny(img,100,200)
+blur = cv2.GaussianBlur(edges,(51,51),0)
 
-plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.subplot(1,3,1),plt.imshow(img),plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+
+plt.subplot(1,3,2),plt.imshow(edges),plt.title('Edges')
 plt.xticks([]), plt.yticks([])
+
+plt.subplot(1,3,3),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+
 plt.show()
